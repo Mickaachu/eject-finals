def0486ca9cc71097dfedf0bbae69571cf600751
Erin Capuchino <valleerin4@gmail.com>
2023-01-12 02:08:22 +0800

    client form connected to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express();
 const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const con = mysql.createConnection({
 	host: 'localhost',
@@ -9,14 +15,26 @@ const con = mysql.createConnection({
 	database:'db_register'
 
 });
-con.query(
-'select * from student',
-function(err,results,fields) {
-	console.log(results);
-	
-}
 
-)
+app.post('/register', (req,res) => {
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
+	const contactNumber = req.body.contactNumber;
+	const address = req.body.address;
+	const oldStudent = req.body.oldStudent;
+	const reason = req.body.reason;
+	console.log(firstName);
+	
+	con.query(
+		'INSERT INTO student (firstName, lastName, email, contactNumber,address, oldStudent, reason) VALUES (?,?,?,?,?,?,?) ',
+		[firstName,lastName,email,contactNumber,address,oldStudent,reason],
+		(err, result) => {
+			console.log(err)
+			console.log(result)
+		})
+
+})
 app.get("/", (req,res) => {
 	
 	res.send('Hello World!')
